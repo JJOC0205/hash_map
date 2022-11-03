f7e2189bc609cabf69a247002d41363af8a43785
rrachelr <98707863+rrachelr@users.noreply.github.com>
2022-11-02 22:04:52 -0400

    Add files via upload

--- HashMap.cpp
+++ HashMap.cpp
@@ -7,6 +7,16 @@
 using namespace std;
 
 hashMap::hashMap(bool hash1, bool coll1) {
+	first = map[0];
+	numKeys = 0;
+	mapSize = 500;
+	hashfn = hash1;
+	collfn = coll1;
+	collisions = 0;
+	hashcoll = 0;
+	for (int i = 0; i < mapSize; i++){
+		map[i] = NULL;
+	}
 }
 void hashMap::addKeyValue(string k, string v) {
 }
@@ -14,10 +24,25 @@ int hashMap::getIndex(string k) {
 }
 
 int hashMap::calcHash2(string k){
+	getClosestPrime();
+	return k%mapSize;
 }
 int hashMap::calcHash1(string k){
+	getClosestPrime();
+	return k%mapSize;
 }
 void hashMap::getClosestPrime() {
+	bool found = false;
+	int size = mapSize*2;
+	while(!found){
+		for (int i = 2; i <= (mapSize); i++){
+			if (!(size % i == 0)){
+				found = true;
+			}
+		}
+		size++;
+	}
+	mapSize = size - 1;
 }
 void hashMap::reHash() {
 }
